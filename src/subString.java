f025ada14f4f402496b6c646fcbc2f8990988162
NAHDI51 <hossainnahdi8051@gmail.com>
2025-12-07 07:04:38 +0600

    Didn't start yet

---
+++ src/subString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class substring {
+
+    /*
+    s = "abcdabcd"
+    t = "abc"
+    t => substring of s
+
+    t = "def"
+    t => not a substring of s
+    checkSubstring => check whether t is a substring of s
+     */
+    public boolean checkSubstring(String s, String t) {
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = 0; j < t.length(); j++) {
+
+            }
+        }
+    }
+}
