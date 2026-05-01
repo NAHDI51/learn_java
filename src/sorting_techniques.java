0c9e7c9793384b9d11fbc13dc0ba8c4b7e6de377
NAHDI51 <hossainnahdi8051@gmail.com>
2026-05-02 03:15:40 +0600

    Lessons of 01.05.2026

--- src/sorting_techniques.java
+++ src/sorting_techniques.java
@@ -79,6 +79,120 @@ public class sorting_techniques {
         }
     }
 
+    public static boolean linearSearch(int[] arr, int key) {
+        for(int i = 0; i < arr.length; i++) {
+            if(key == arr[i]) {
+                return true;
+            }
+        }
+        // x -> each element of arr
+        // Enhanced loop
+        for(int x : arr) {
+            if(key == x) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Recursive implementation
+    public static boolean binarySearchRecursive(int[] arr, int low, int high, int key) {
+        if(low > high) {
+            return false;
+        }
+        // Average of low and high
+        int mid = (low + high) / 2;
+        if(arr[mid] == key) {
+            // we found the key
+            return true;
+        } else if(arr[mid] < key) {
+            // Go right
+            return binarySearchRecursive(arr, mid+1, high, key);
+        } else {    // arr[mid] > key
+            return binarySearchRecursive(arr, low, mid-1, key);
+        }
+    }
+
+    // Iterative implementation
+    public static boolean binarySearchIterative(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length-1;
+
+        // low > high -> false
+        while(low <= high) {
+//            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
+
+            // check the current lement
+            if(arr[mid] == key) {
+                return true;
+            } else if(arr[mid] < key) {
+                low = mid + 1;
+            } else { // arr[mid] > key
+                high = mid - 1;
+            }
+        }
+        // low > high
+        return false;
+    }
+
+    public static void merge(int[] a, int low, int mid, int high) {
+        // Arrays are so far empty, we need to fill them with the
+        // value of a
+        int[] left = new int[mid-low+1];
+        int[] right = new int[high-mid];
+
+        // Assign corresponding values of a;
+        for(int i = 0; i < left.length; i++) {
+            left[i] = a[low+i];
+        }
+        for(int i = 0; i < right.length; i++) {
+            right[i] = a[mid+i+1];
+        }
+
+        // Merging process
+        int k = low;    // Index of the main array
+        int li = 0;     // Left Index
+        int ri = 0;     // Right Index
+
+        // While we have elements to pick from either left or right
+        // if left remains OR right remains
+        while(li != left.length || ri != right.length) {
+            // If we have picked all elements from left already
+            if(li == left.length) {
+                a[k] = right[ri];
+                ri++;
+            } else if(ri == right.length) {
+            // If we have picked all elements from right already
+                a[k] = left[li];
+                li++;
+            } else if(left[li] < right[ri]) {
+            // At this point, none of the array has been taken fully.
+            // So, compare, and take the smaller one
+                a[k] = left[li];
+                li++;
+            } else {
+                a[k] = right[ri];
+                ri++;
+            }
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] a, int low, int high) {
+        // if(low == high) then the array is already sorted
+        if(low < high) {
+            int mid = (low + high) / 2;     // mid = average(low, high)
+            // Sorts the array from low to mid
+            mergeSort(a, low, mid);
+            // Sorts the array from mid+1 to high
+            mergeSort(a, mid+1, high);
+
+            // Finally, merge the two partitions
+            merge(a, low, mid, high);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
@@ -95,11 +209,20 @@ public class sorting_techniques {
         System.out.println(" ");
 
 //        selection_sort(arr);
-        insertion_sort(arr);
+//        insertion_sort(arr);
+        mergeSort(arr, 0, arr.length-1);
         System.out.print("The array after sorting: ");
         for(int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println(" ");
+
+//        boolean found = binarySearchRecursive(arr, 0, arr.length-1, 10);
+        boolean found = binarySearchIterative(arr, 9);
+        if(found) {
+            System.out.println("The integer is found!");
+        } else {
+            System.out.println("The integer is not found!");
+        }
     }
 }
